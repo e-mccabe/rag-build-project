a0c08d739dc7537d1068693613d9b814b5c65442
metasisyphus <eoinmccabe.finance@gmail.com>
2026-07-28 19:29:37 +0100

    added defensive case to ask()

--- src/rag_build/response.py
+++ src/rag_build/response.py
@@ -1,30 +1,35 @@
 from openai import OpenAI
 
 from rag_build.querying import search,rerank
-from rag_build.utils import _generate_numbered_context_strings
+from rag_build.utils import generate_numbered_context_strings
 from rag_build.config import RESPONSE_MODEL,SYSTEM_PROMPT
-
 
 _client = OpenAI()
 
-def retrieval(question:str, **search_kwargs) -> list[str]:
+def retrieval(question:str, **search_kwargs) -> list[dict]:
+    """Based of user query generate the relevant chunks for the vector database"""
 
     hits = search(query=question,**search_kwargs)
 
     if not hits:
-        return {'answer':"I can't have anything in the notes about that",
-                'sources':[]}
+        return []
     
     return rerank(question,hits)
 
-
-def ask(question: str,**search_kwargs) -> str:
+def ask(question: str,**search_kwargs) -> dict:
 
     hits = retrieval(question,**search_kwargs)
 
-    context_string = _generate_numbered_context_strings(hits)
+    if not hits:
+        return {
+            'answer':'No Answer Found in Corpus',
+            'sources':['N/A'],
+            'ids':['N/A']
+        }
 
-    input =  f'User Question: {question}. **Retrieved Context: {context_string}'
+    context_string = generate_numbered_context_strings(hits)
+
+    prompt =  f'User Question: {question}. **Retrieved Context: {context_string}'
 
 
     response = _client.chat.completions.create(
@@ -32,34 +37,33 @@ def ask(question: str,**search_kwargs) -> str:
         max_tokens=500,
         messages=[
             {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": input}
+            {"role": "user", "content": prompt}
         ],
         )
     
     sources = [f'{hit['metadata']['source']}/{'/'.join(hit['metadata']['headings'].split(','))}' for hit in hits]
-
+    ids = [hit['id'] for hit in hits]
     return {
         'answer':response.choices[0].message.content,
-        'sources':sources
+        'sources':sources,
+        'ids':ids
     }
 
+def generate_stream(question: str,hits:list[dict]):
 
-def generate_stream(question: str,hits:list[str]) -> str:
+    context_string = generate_numbered_context_strings(hits)
 
-    context_string = _generate_numbered_context_strings(hits)
-
-    input =  f'User Question: {question}. **Retrieved Context: {context_string}'
+    prompt =  f'User Question: {question}. **Retrieved Context: {context_string}'
 
     stream = _client.chat.completions.create(
          model=RESPONSE_MODEL,
         max_tokens=500,
         messages=[
             {"role": "system", "content": SYSTEM_PROMPT},
-            {"role": "user", "content": input}
+            {"role": "user", "content": prompt}
         ],
         stream=True
         )
     
     for piece in stream:
         yield piece
-
